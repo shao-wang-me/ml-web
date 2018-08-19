7b91e2b4f16628cac4d6672ee4cb43a762cfba2d
Shao Wang <wsdonny@gmail.com>
2018-08-20 01:42:05 +1000

    Reorganise sampler.py

--- sampler.py
+++ sampler.py
@@ -1,20 +1,30 @@
 import tweepy
-
-consumer_key = None
-consumer_secret = None
-access_token = None
-access_token_secret = None
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+import sys
+import sqlite3
 
 
 class Listener(tweepy.StreamListener):
-    pass
+    def on_data(self, data):
+        print(data)
+        return True
+
+    def on_error(self, status):
+        print(status)
 
 
-streamListener = Listener()
-stream = tweepy.Stream(auth=auth, listener=streamListener)
+if __name__ == '__main__':
+    consumer_key = sys.argv[1]
+    consumer_secret = sys.argv[2]
+    access_token = sys.argv[3]
+    access_token_secret = sys.argv[4]
 
-stream.sample(async=True)
-# stream.filter(async=True)
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    streamListener = Listener()
+    stream = tweepy.Stream(auth=auth, listener=streamListener)
+
+    conn = sqlite3.connect('twitter.db')
+    c = conn.cursor()
+    stream.sample()
+    # stream.filter()
